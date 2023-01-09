af5ddf0caba1eabe010bbd8fa5c32f314288a8ae
user <abhayhari17@gmail.com>
2023-01-09 10:13:11 +0530

    change Icon to

---
+++ lib/jsonPlaceholder.dart
@@ -0,0 +1,29 @@
+// TODO Implement this library.import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'JsonPlaceHolderView.dart';
+class JsonPlaceHolderView extends JsonPlaceHolderViewModel {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: isLoading ? CircularProgressIndicator(
+            valueColor: AlwaysStoppedAnimation<Color>(Colors.white))
+            : Text("Jsonplaceholder"),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () =>  getPost(),
+        child: const Icon(Icons.favorite),
+      ),
+      body: ListView.builder(
+          itemCount: postModel.length,
+          itemBuilder: (context, index) => Card(
+            child: ListTile(
+              title: Text(postModel[index].title.toString()),
+              subtitle: Text(postModel[index].body.toString()),
+              leading: Text(postModel[index].id.toString()),
+            ),
+          )),
+    );
+  }
+}
---
+++ lib/jsonpage.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+
+import 'jsonPlaceholder.dart';
+
+
+
+class JsonPlaceHolder extends StatefulWidget {
+  @override
+  JsonPlaceHolderView createState() => JsonPlaceHolderView() ;
+}
